27909308040b4f75f9335b6c740928acb9b7bcaf
Rebecca Pae <r.pae92@gmail.com>
2019-06-14 22:01:00 -0700

    Added model and fixed totaldifference calc

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,16 +3,16 @@ var friends = [
     name: "Matt",
     "photo": "linktophoto",
     "scores": [
-        5,
+        2,
         3,
+        1,
+        5,
+        1,
         2,
         4,
         1,
-        2,
-        3,
         4,
-        3,
-        2
+        1
         ]
     },
     {
@@ -20,17 +20,33 @@ var friends = [
         "photo": "photolink",
         "scores": [
             2,
-            4,
-            5,
-            1,
-            1,
-            2,
-            4,
-            2,
             3,
-            1
+            2,
+            2,
+            4,
+            4,
+            2,
+            2,
+            5,
+            2
             ]
-        }
+        },
+        {
+            name: "Angie",
+            "photo": "photo",
+            "scores": [
+                1,
+                2,
+                1,
+                4,
+                4,
+                3,
+                3,
+                2,
+                5,
+                2
+                ]
+            }
 ];
 
 module.exports = friends;
